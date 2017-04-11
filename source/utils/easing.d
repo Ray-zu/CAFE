e3bfe612802dc0ce478a9b9c894a6fcd200c31dd
aoitofu <aoitofu@dr.com>
2017-04-11 19:12:06 +0900

    イージング計算関数を追加

--- source/utils/easing.d
+++ source/utils/easing.d
@@ -16,6 +16,9 @@
 +/
 
 module cafe.utils.easing;
+import cafe.types;
+
+debug = 1;
 
 /+ イージングの種類を表します。
    イージングの詳細については下記URLを参考にしてください。
@@ -23,7 +26,7 @@ module cafe.utils.easing;
 +/
 enum EasingType
 {
-    Liner,
+    Linear,
     QuadraticEasingIn,
     QuadraticEasingOut,
     QuadraticEasingIO,
@@ -45,4 +48,51 @@ enum EasingType
     CircularEasingIn,
     CircularEasingOut,
     CircularEasingIO
+}
+
+
+/+ イージング計算中の例外 +/
+class CalculateEasingFault : Error
+{
+    public this ( string mes, string file = __FILE__, uint line = __LINE__ )
+    {
+        super( mes, file, line );
+    }
+}
+
+/+ 名前長いわ! +/
+private alias error = CalculateEasingFault;
+
+
+/+ フレーム数、開始値、終了値からイージングされた中間値を求めます。
+   type : イージングの種類
+   cur  : 現在のフレーム数
+   dur  : 総フレーム数
+   st   : 開始値
+   add  : 加算値 ( st + addが最終的な結果になる )
++/
+float calcEasing ( EasingType type, Frame cur, FrameLen dur, float st, float add )
+{
+    enum OverFrameIndex = "frame index is over than duration";
+
+    if ( cur >= dur ) throw new error( OverFrameIndex );
+
+    switch ( type ) {
+        case EasingType.Linear: /+ 直線移動 +/
+            return add * (cur*1.0) / (dur*1.0) + st;
+
+        case EasingType.QuadraticEasingIn:
+            float t = (cur*1.0) / (dur*1.0);
+            return add*t*t + st;
+
+        default: throw new Exception( "Not Implemented" );
+    }
+}
+
+
+/+ イージング関数のテスト +/
+debug ( 1 ) unittest {
+    import std.stdio;
+    assert( calcEasing( EasingType.Linear           , 50, 100, 50, 50 ) == 75 );
+    assert( calcEasing( EasingType.QuadraticEasingIn, 50, 100, 50, 50 ) == 62.5 );
 }
