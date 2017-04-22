29dcb002899ce92806c6613893fb86319ecd737f
aoitofu <aoitofu@dr.com>
2017-04-22 21:11:43 +0900

    コード短縮化の為累乗式をpow関数に変更

--- source/utils/easing.d
+++ source/utils/easing.d
@@ -16,6 +16,7 @@
 +/
 
 module cafe.utils.easing;
+import std.math;
 import cafe.types;
 
 debug = 1;
@@ -86,52 +87,52 @@ float calcEasing ( EasingType type, Frame cur, FrameLen dur, float st, float add
             return add * (cur*1.0) / (dur*1.0) + st;
 
         case EasingType.QuadraticEasingIn:
-            return add*ratio*ratio + st;
+            return add*ratio.pow(2) + st;
 
         case EasingType.QuadraticEasingOut:
             return -add*ratio*(ratio-2) + st;
 
         case EasingType.QuadraticEasingIO:
-            if ( ratio < 1 ) return add/2*ratio*ratio + st;
+            if ( ratio < 1 ) return add/2*ratio.pow(2) + st;
             ratio--;
             return -add/2 * (ratio*(ratio-2) - 1) + st;
 
         case EasingType.CubicEasingIn:
-            return add*ratio*ratio*ratio + st;
+            return add*ratio.pow(3) + st;
 
         case EasingType.CubicEasingOut:
             ratio--;
-            return add*(ratio*ratio*ratio + 1) + st;
+            return add*(ratio.pow(3) + 1) + st;
 
         case EasingType.CubicEasingIO:
-            if ( ratio < 1 ) return add/2*ratio*ratio*ratio + st;
+            if ( ratio < 1 ) return add/2*ratio.pow(3) + st;
             ratio -= 2;
-            return add/2*(ratio*ratio*ratio + 2) + st;
+            return add/2*(ratio.pow(3) + 2) + st;
 
         case EasingType.QuarticEasingIn:
-            return add*ratio*ratio*ratio*ratio + st;
+            return add*ratio.pow(4) + st;
 
         case EasingType.QuarticEasingOut:
             ratio--;
-            return -add * (ratio*ratio*ratio*ratio - 1) + st;
+            return -add * (ratio.pow(4) - 1) + st;
 
         case EasingType.QuarticEasingIO:
-            if ( ratio < 1 ) return add/2*ratio*ratio*ratio*ratio + st;
+            if ( ratio < 1 ) return add/2*ratio.pow(4) + st;
             ratio -= 2;
-            return -add/2 * (ratio*ratio*ratio*ratio - 2) + st;
+            return -add/2 * (ratio.pow(4) - 2) + st;
 
         case EasingType.QuinticEasingIn:
-            return add*ratio*ratio*ratio*ratio*ratio;
+            return add*ratio.pow(5);
 
         case EasingType.QuinticEasingOut:
             ratio--;
-            return add * (ratio*ratio*ratio*ratio*ratio + 1) + st;
+            return add * (ratio.pow(5) + 1) + st;
 
         case EasingType.QuinticEasingIO:
             ratio /= 2;
-            if ( ratio < 1 ) return add/2*ratio*ratio*ratio*ratio*ratio + st;
+            if ( ratio < 1 ) return add/2*ratio.pow(5) + st;
             ratio -= 2;
-            return add/2 * (ratio*ratio*ratio*ratio*ratio + 2) + st;
+            return add/2 * (ratio.pow(5) + 2) + st;
 
             /+ TODO +/
         default: throw new Exception( "Not Implemented" );
