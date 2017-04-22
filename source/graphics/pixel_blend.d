aee3bb7e707456bba80202738906a1c5b3af7404
aoitofu <aoitofu@dr.com>
2017-04-22 20:47:09 +0900

    ピクセルの通常ブレンド関数を追加

--- source/graphics/pixel_blend.d
+++ source/graphics/pixel_blend.d
@@ -16,6 +16,9 @@
 +/
 
 module cafe.graphics.pixel_blend;
+import std.algorithm,
+       std.conv;
+import cafe.graphics.pixel;
 
 debug = 1;
 
@@ -41,4 +44,40 @@ debug ( 1 ) unittest {
 }
 
 
-/+  +/
+/+ 冗長なコードを避けるために共通部分を文字列化し、mixinできるようにします。 +/
+enum CommonVars = q{
+    auto result = Pixel();
+    auto Fa = fg.a.floating;
+    auto Fr = fg.r.floating;
+    auto Fg = fg.g.floating;
+    auto Fb = fg.b.floating;
+    auto Ba = bg.a.floating;
+    auto Br = bg.r.floating;
+    auto Bg = bg.g.floating;
+    auto Bb = bg.b.floating;
+    auto Ca = Fa+(1-Fa)*Ba;
+};
+
+
+/+ 通常モードでブレンドします。 +/
+Pixel blendNormal ( Pixel fg, Pixel bg )
+{
+    mixin( CommonVars );
+    result.a = (Ca * 255).to!short;
+    if ( Ca != 0 ) {
+        result.r = ((( Fa * Ba * Fr + Fa * (1-Ba) * Fr + (1-Fa) * Ba * Br ) / Ca) * 255).to!short;
+        result.g = ((( Fa * Ba * Fg + Fa * (1-Ba) * Fg + (1-Fa) * Ba * Bg ) / Ca) * 255).to!short;
+        result.b = ((( Fa * Ba * Fb + Fa * (1-Ba) * Fb + (1-Fa) * Ba * Bb ) / Ca) * 255).to!short;
+    }
+    return result;
+}
+
+debug ( 1 ) unittest {
+    Pixel bg = { r:255, g:0, b:0, a:255 };
+    Pixel fg = { r:0, g:0, b:255, a:(0.5*ubyte.max).to!short };
+    auto result = blendNormal( fg, bg );
+    assert( result.r == 128 );
+    assert( result.g == 0 );
+    assert( result.b == 127 );
+    assert( result.a == 255 );
+}
