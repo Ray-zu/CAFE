88416bc4a8580444d0f2ad779d64beadd82e1734
aoitofu <aoitofu@dr.com>
2017-05-18 22:57:52 +0900

    プロパティテーブルを管理するクラスを追加

---
+++ source/gui/common/PropertyTable.d
@@ -0,0 +1,17 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.gui.common.PropertyTable;
+import dlangui;
+
+/+ タイムラインオブジェクトの値を編集するテーブル +/
+class PropertyTable
+{
+    /+ セルのイベントデリゲートとプロパティ名等を保存するクラス +/
+    public class CellInfo
+    {
+    }
+}
