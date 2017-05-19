8282828019eec67617c19ac113262eddd567cfd3
aoitofu <aoitofu@dr.com>
2017-05-20 08:22:56 +0900

    テストオブジェクトを追加

---
+++ source/project/timeline/TestObject.d
@@ -0,0 +1,18 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.project.timeline.TestObject;
+import cafe.project.timeline.PlaceableObject,
+       cafe.project.timeline.PropertyKeepableObject;
+
+class TestObject
+{
+    mixin PlaceableObject;
+
+    unittest {
+        auto hoge = new TestObject;
+    }
+}
