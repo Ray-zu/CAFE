47f12c37e3075433fc1d0c1b5e2e455db47e8da7
aoitofu <aoitofu@dr.com>
2017-05-21 19:34:33 +0900

    プロパティクラスをリセット

--- source/project/timeline/PropertyKeepableObject.d
+++ source/project/timeline/PropertyKeepableObject.d
@@ -8,34 +8,6 @@ module cafe.project.timeline.PropertyKeepableObject;
 import std.conv,
        std.traits;
 
-/+ プロパティ情報 +/
-abstract class Property ( T )
-    if ( isScalarType!T )
-{
-    private:
-        T val;
-
-    public:
-        @property value () { return val; }
-
-        /+ 文字列からプロパティを更新 +/
-        void fromString ( string v )
-        {
-            val = v.to!T;
-        }
-
-        /+ プロパティを文字列化 +/
-        override @property string toString ()
-        {
-            return val.to!string;
-        }
-}
-
-/+ 数値プロパティ +/
-class NumericProperty ( T ) : Property!T
-    if ( isNumericType!T )
-{
-}
 
 /+ プロパティを保持できるオブジェクトの共通部分 +/
 template PropertyKeepableObject (  )
