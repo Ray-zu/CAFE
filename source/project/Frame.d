3abe3f7904a58724a4cf9dbda3e799b3e5d5bc5c
aoitofu <aoitofu@dr.com>
2017-05-22 21:58:53 +0900

    フレーム数関連を表すクラスを追加

--- source/project/Frame.d
+++ source/project/Frame.d
@@ -5,69 +5,144 @@
  + Please see /LICENSE.                                         +
  + ------------------------------------------------------------ +/
 module cafe.project.Frame;
-import std.conv;
+import std.conv,
+       std.format;
 
-/+ フレーム数を表します +/
-class FrameLength
+/+ フレーム数関係を表すクラスの共通部分 +/
+private template FrameTime ()
 {
-    public:
-        uint frame;
-        alias frame this;
+    private:
+        uint val;
 
-        this ( uint i )
+    public:
+        @property value ()         { return val; }
+        @property value ( uint f ) { val = f;    }
+
+        this ( uint f )
         {
-            frame = i;
+            value = f;
         }
 
-        unittest {
-            auto t = new FrameLength( 5 );
-            assert( cast(uint)t == 5 );
+        override @property string toString ()
+        {
+            return value.to!string;
         }
 }
 
-/+ ある1フレームを表します +/
-class FrameCount
+/+ フレーム数(長さ)                      +
+ + 0から始まるフレーム総数を表現します。 +
+ + 例:シーン全体の長さ                   +/
+class FrameLength
+{
+    mixin FrameTime;
+    unittest {
+        auto t = new FrameLength( 500 );
+        assert( t.value == 500 );
+    }
+}
+
+/+ フレーム数(一点)             +
+ + ある１フレームを表現します。 +
+ + 例:再生中フレーム            +/
+ class FrameAt
+{
+    mixin FrameTime;
+    unittest {
+        auto f = new FrameAt( 120 );
+        assert( f.value == 120 );
+    }
+}
+
+/+ フレーム数(一点)              +
+ + ある一フレームを表現します。  +
+ + FrameLength情報も保存します。 +/
+ class FrameIn : FrameAt
 {
     private:
         FrameLength total;
-        uint frame;
+
+        auto valueNormalize ( uint f )
+        {
+            if ( length.value >= f ) f = length.value - 1;
+            if ( 0 < f ) f = 0;
+            return f;
+        }
 
     public:
-        @property value () { return frame; }
-        @property value ( uint f )
+        @property uint value ()
         {
-            if ( cast(uint)total >= f ) frame = f;
-            else throw new
-                Exception( "The time is over than total." );
+            return valueNormalize(value);
+        }
+        @property void value ( uint f )
+        {
+            super.value = valueNormalize(f);
         }
 
-        @property valueRatio ()
-        {
-            return value.to!float / (cast(uint)total).to!float;
-        }
+        @property length () { return total; }
 
-        @property hasTotal ()
-        {
-            return total !is null;
-        }
-
-        this ( uint i )
-        {
-            total = null;
-            frame = i;
-        }
-
-        this ( FrameLength t, uint i )
+        this ( FrameLength t, uint f )
         {
             total = t;
-            frame = i;
+            super( f );
         }
 
         unittest {
-            auto hoge = new FrameCount( new FrameLength( 100 ), 50 );
-            assert( hoge.value == 50 );
-            assert( hoge.valueRatio == 0.5 );
-            hoge.value = 75;
-            assert( hoge.valueRatio == 0.75 );
+            auto f = new FrameIn( new FrameLength(500), 100 );
+            assert( f.value == 100 );
+            assert( f.length.value == 500 );
+        }
+}
+
+/+ フレーム数(期間)                                  +
+ + 任意のフレームから始まるフレーム総数を表現します。
+ + オブジェクトのタイムライン上での位置+/
+ class FramePeriod
+{
+    private:
+        FrameLength total;
+        FrameAt start_frame;
+        FrameAt end_frame;
+
+        FrameAt frameNormalize ( FrameAt f ) // 型指定しないとコンパイルエラー(バグ？)
+        {
+            if ( f.value >= length.value ) f.value = length.value - 1;
+            if ( f.value < 0 ) f.value = 0;
+            return f;
+        }
+
+    public:
+        @property parentLength () { return total;                       }
+        @property start        () { return frameNormalize(start_frame); }
+        @property end          () { return frameNormalize(end_frame  ); }
+
+        @property start ( FrameAt f )
+        {
+            if ( f.value >= end_frame.value ) throw new Exception( "Invalid Period" );
+            start_frame = frameNormalize(f);
+        }
+        @property end ( FrameAt f )
+        {
+            if ( f.value <= start_frame.value ) throw new Exception( "Invalid Period" );
+            end_frame = frameNormalize(f);
+        }
+
+        this ( FrameLength t, FrameAt s, FrameAt e )
+        {
+            total = t;
+            start = s;
+            end = e;
+        }
+
+        @property length ()
+        {
+            return new FrameLength( end.value - start.value );
+        }
+
+        unittest {
+            auto f = new FramePeriod( new FrameLength(100), new FrameAt(20), new FrameAt(50) );
+            assert( f.parentLength.value == 100 );
+            assert( f.start.value == 20 );
+            assert( f.end.value == 50 );
+            assert( f.length.value == 30 );
         }
 }
