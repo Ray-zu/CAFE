dbf26b4f2fa11e6be5c71847ce82ab3f30826121
aoitofu <aoitofu@dr.com>
2017-05-23 18:48:40 +0900

    イージング関数関連の名前空間を追加

---
+++ source/project/timeline/property/Easing.d
@@ -0,0 +1,9 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.project.timeline.property.Easing;
+
+// TODO
