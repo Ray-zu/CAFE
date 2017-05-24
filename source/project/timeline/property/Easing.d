4767a414584a004b32f87a82d55c3c76f8485163
aoitofu <aoitofu@dr.com>
2017-05-24 16:40:30 +0900

    イージング関連で一次関数クラスを追加

--- source/project/timeline/property/Easing.d
+++ source/project/timeline/property/Easing.d
@@ -6,6 +6,7 @@
  + ------------------------------------------------------------ +/
 module cafe.project.timeline.property.Easing;
 import cafe.project.ObjectPlacingInfo;
+import std.conv;
 
 /+ イージング関数のベースクラス +/
 abstract class EasingFunction
@@ -30,5 +31,43 @@ abstract class EasingFunction
             dur = d;
         }
 
+        @property changeInValue ()
+        {
+            return end - start;
+        }
+
+        @property slope ()
+        {
+            return changeInValue/duration.value.to!float;
+        }
+
         float at ( FrameAt f );
+}
+
+/+ イージング関数クラスの共通コンストラクタ +/
+private template EasingFunctionConstructor ()
+{
+    public:
+        this ( float s, float e, FrameLength d )
+        {
+            super( s, e, d );
+        }
+}
+
+/+ simple linear tweening +/
+class LinearEasing : EasingFunction
+{
+    mixin EasingFunctionConstructor;
+    public:
+        override float at ( FrameAt current )
+        {
+            auto c = current.value;
+            return slope*c + start;
+        }
+
+        unittest {
+            import std.stdio;
+            auto hoge = new LinearEasing( 0, 75, new FrameLength(20) );
+            assert( hoge.at( new FrameAt(10) ) == 37.5 );
+        }
 }
