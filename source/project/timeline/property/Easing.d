19b77bba2d7fe6866c334a3d7d41986f17b38e69
aoitofu <aoitofu@dr.com>
2017-05-24 16:23:28 +0900

    イージング関数のベースクラスを追加

--- source/project/timeline/property/Easing.d
+++ source/project/timeline/property/Easing.d
@@ -5,5 +5,30 @@
  + Please see /LICENSE.                                         +
  + ------------------------------------------------------------ +/
 module cafe.project.timeline.property.Easing;
+import cafe.project.ObjectPlacingInfo;
 
-// TODO
+/+ イージング関数のベースクラス +/
+abstract class EasingFunction
+{
+    private:
+        float       st_value;
+        float       ed_value;
+        FrameLength dur;
+
+    public:
+        @property start    () { return st_value; }
+        @property end      () { return ed_value; }
+        @property duration () { return dur;      }
+
+        @property start ( float s ) { st_value = s; }
+        @property end   ( float e ) { ed_value = e; }
+
+        this ( float s, float e, FrameLength d )
+        {
+            start = s;
+            end = e;
+            dur = d;
+        }
+
+        float at ( FrameAt f );
+}
