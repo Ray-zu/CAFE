50e9e6ea15b87991e38df175122f44043ddde7a0
aoitofu <aoitofu@dr.com>
2017-05-27 23:38:09 +0900

    プロパティクラスを追加

---
+++ source/project/timeline/property/Property.d
@@ -0,0 +1,62 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.project.timeline.property.Property;
+import cafe.project.ObjectPlacingInfo,
+       cafe.project.timeline.property.MiddlePoint;
+
+debug = 1;
+
+/+ プロパティデータのインターフェース +/
+interface Property
+{
+    public:
+        @property FrameLength   frame        ();
+        @property MiddlePoint[] middlePoints ();
+
+        /+ ユーザーの入力した文字列をプロパティに変換 +/
+        void   set ( FrameAt, string );
+        string get ( FrameAt );
+}
+
+/+ プロパティデータ +/
+class PropertyBase (T) : Property
+{
+    private:
+        alias MPoint = MiddlePointBase!T;
+
+        FrameLength frame_len;
+        MPoint[]    middle_points;
+        T           end_value;
+
+    public:
+        override @property FrameLength   frame        () { return frame_len;     }
+        override @property MiddlePoint[] middlePoints () { return middle_points; }
+                 @property T             endValue     () { return end_value;     }
+
+        this ( FrameLength f, T v )
+        {
+            frame_len = f;
+            middle_points = [new MPoint( v,
+                    new FramePeriod( f, new FrameAt(0), new FrameAt(f.value-1) ) )];
+            end_value = v;
+        }
+
+        override void set ( FrameAt f, string v )
+        {
+            throw new Exception( "Not Implemented" );
+        }
+
+        override string get ( FrameAt f )
+        {
+            throw new Exception( "Not Implemented" );
+        }
+
+        debug ( 1 ) unittest {
+            auto hoge = new PropertyBase!float( new FrameLength(50), 20 );
+            assert( hoge.middlePoints.length == 1 );
+        }
+}
