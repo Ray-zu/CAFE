e2e2fca0bba86b93a33ddaddd6a932a5db0bc7a3
aoitofu <aoitofu@dr.com>
2017-05-29 07:44:31 +0900

    templateを抽象クラスに修正

--- source/project/timeline/PlaceableObject.d
+++ source/project/timeline/PlaceableObject.d
@@ -9,7 +9,7 @@ import std.conv;
 
 /+ タイムラインに配置できるオブジェクト                +
  + 主にオブジェクトの開始/終了フレームやレイヤ数を管理 +/
-template PlaceableObject ()
+abstract class PlaceableObject ()
 {
     import cafe.project.ObjectPlacingInfo;
 
