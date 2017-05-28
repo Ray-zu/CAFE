834baf004000a2b75ceeac7c6b371172b057b8c0
aoitofu <aoitofu@dr.com>
2017-05-29 07:27:19 +0900

    プロジェクト全体のデータクラスを追加

---
+++ source/project/Project.d
@@ -0,0 +1,14 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.project.Project;
+
+debug = 0;
+
+/+ プロジェクト全体のデータ +/
+class Project
+{
+}
