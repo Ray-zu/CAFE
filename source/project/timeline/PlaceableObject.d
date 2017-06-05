fbba4475081d9d800d03d8917037f27ca2976cc9
aoitofu <aoitofu@dr.com>
2017-06-05 10:46:24 +0900

    配置可能なオブジェクトの共通部分を追加

--- source/project/timeline/PlaceableObject.d
+++ source/project/timeline/PlaceableObject.d
@@ -6,3 +6,26 @@
  + ------------------------------------------------------------ +/
 module cafe.project.timeline.PlaceableObject;
 import cafe.project.ObjectPlacingInfo;
+
+/+ タイムラインに配置可能なオブジェクトのインターフェース          +
+ + クラスの多重継承が出来ないのでテンプレートで実体が定義されます。+/
+interface PlaceableObject
+{
+    public:
+        @property ObjectPlacingInfo place ();
+}
+
+
+
+/+ Placeableobjectの実体 +/
+template PlaceableObjectCommon
+{
+    private:
+        ObjectPlacingInfo placing_info;
+
+    public:
+        override @property ObjectPlacingInfo place ()
+        {
+            return placing_info;
+        }
+}
