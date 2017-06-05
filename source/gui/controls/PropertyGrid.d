c3e21f59be40fbc36c715a03fced60f312ef819e
aoitofu <aoitofu@dr.com>
2017-06-05 17:24:43 +0900

    プロパティグリッドクラスを追加

--- source/gui/controls/PropertyGrid.d
+++ source/gui/controls/PropertyGrid.d
@@ -5,3 +5,42 @@
  + Please see /LICENSE.                                         +
  + ------------------------------------------------------------ +/
 module cafe.gui.controls.PropertyGrid;
+import cafe.project.timeline.property.PropertyList;
+import dlangui;
+
+/+ プロパティグリッド +/
+class PropertyGrid : StringGridWidget
+{
+    private:
+        PropertyList props;
+
+    public:
+        @property propertyList () { return props; }
+        @property propertyList ( PropertyList p )
+        {
+            props = p;
+            gridUpdate;
+        }
+
+        this ( string id = "PropertyGrid" )
+        {
+            super( id );
+            layoutWidth( FILL_PARENT ).layoutHeight( FILL_PARENT );
+            propertyList = null;
+        }
+
+        /+ propertyListに従ってグリッドを更新 +/
+        void gridUpdate ()
+        {
+            if ( propertyList ) {
+                resize( 1, propertyList.properties.length );
+                enabled = true;
+
+            } else {
+                resize( 1, 1 );
+                setCellText( 0, 0, "" );
+                enabled = false;
+            }
+            autoFit;
+        }
+}
