2f82327ae8703d3bf9aa74845638fa3af374a23b
aoitofu <aoitofu@dr.com>
2017-06-08 21:56:17 +0900

    ポリゴンクラスを追加

---
+++ source/renderer/polygon/Polygon.d
@@ -0,0 +1,22 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.renderer.polygon.Polygon;
+import cafe.renderer.graphics.Bitmap;
+
+debug = 1;
+
+/+ ポリゴンのデータ +/
+class Polygon
+{
+    private:
+        BMP bmp;
+
+    public:
+        @property bitmap () { return bmp; }
+
+        // TODO : コンストラクタの追加
+}
