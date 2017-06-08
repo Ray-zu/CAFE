9b7aabe4e1e13ab375a3036252cd574396273d90
aoitofu <aoitofu@dr.com>
2017-06-08 22:57:34 +0900

    カメラクラスを追加

---
+++ source/renderer/Camera.d
@@ -0,0 +1,26 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.renderer.Camera;
+import cafe.renderer.polygon.Vector;
+
+debug = 0;
+
+/+ カメラクラス                  +
+ + WorldクラスをBMPに変換します。+/
+class Camera
+{
+    private:
+        Vector3D pos;
+        Vector3D tarpos;
+        // TODO : レンダラークラスのインスタンス
+
+    public:
+        @property position () { return pos; }
+        @property targetPosition () { return tarpos; }
+
+        // TODO : コンストラクタ
+}
