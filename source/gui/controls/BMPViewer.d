1880f126816619e38e49137f62a22fbb247fd12f
aoitofu <aoitofu@dr.com>
2017-06-11 14:58:03 +0900

    BMPViewerにBMPの描画処理を追加

--- source/gui/controls/BMPViewer.d
+++ source/gui/controls/BMPViewer.d
@@ -9,7 +9,8 @@ import cafe.renderer.graphics.Bitmap,
        cafe.renderer.graphics.Color;
 import dlangui;
 
-/+ BMPを表示するウィジェット +/
+/+ BMPを表示するウィジェット                                    +
+ + ピクセル操作してるのでウィジェット大きくするとやばい(やばい) +/
 class BMPViewer : Widget
 {
     private:
@@ -17,14 +18,36 @@ class BMPViewer : Widget
 
     public:
         @property bitmap () { return bmp; }
+        @property bitmap ( BMP bmp )
+        {
+            this.bmp = bmp;
+            invalidate;
+        }
 
         this ( string id, BMP bmp = null )
         {
+            // テストコード
+            bmp = new BMP( 10, 10 );
+            bmp[5,5] = RGBA( 255, 255, 255 );
+
             super( id );
-            this.bmp = bmp;
+            bitmap = bmp;
         }
 
         override void onDraw ( DrawBuf b )
         {
+            if ( !bitmap ) return;
+
+            int dx, dy;
+            int bx, by;
+            auto bitw_per_pixelw = (bitmap.width.to!float/width.to!float);
+            auto bith_per_pixelh = (bitmap.height.to!float/height.to!float);
+
+            for ( dy=0; dy < height; dy++ )
+                for ( dx=0; dx < width; dx++ ) {
+                    bx = (bitw_per_pixelw * dx).to!int;
+                    by = (bith_per_pixelh * dy).to!int;
+                    b.drawPixel( dx, dy, bitmap[bx,by].toHex );
+                }
         }
 }
