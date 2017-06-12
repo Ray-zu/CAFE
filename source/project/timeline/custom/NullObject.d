de66634211878959b3af1094bf333e4e8f798925
aoitofu <aoitofu@dr.com>
2017-06-12 19:18:35 +0900

    NullObjectを追加

---
+++ source/project/timeline/custom/NullObject.d
@@ -0,0 +1,40 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.project.timeline.custom.NullObject;
+import cafe.project.timeline.PlaceableObject;
+
+debug = 1;
+
+/+ デバッグ用の何もしないオブジェクト +/
+class NullObject : PlaceableObject
+{
+    mixin EffectKeepableObjectCommon;
+    public:
+        override @property PlaceableObject copy ()
+        {
+            return new NullObject( this );
+        }
+
+        this ( NullObject src )
+        {
+            super( src );
+            copyEffectFrom( src );
+        }
+
+        this ()
+        {
+            super();
+        }
+
+        override void initProperties ()
+        {
+        }
+
+        debug (1) unittest {
+            auto hoge = new NullObject;
+        }
+}
