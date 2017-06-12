33359aff8a81a99cc2d06ed734d960eaedfbf3da
aoitofu <aoitofu@dr.com>
2017-06-12 16:04:23 +0900

    BMPクラスをDrawBufへ変換するBitmapLightクラスを追加

---
+++ source/gui/BitmapLight.d
@@ -0,0 +1,25 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.gui.BitmapLight;
+import cafe.renderer.graphics.Bitmap;
+import dlangui;
+
+debug = 0;
+
+/+ BMPViewerコンポーネントに直接渡せるBitmapクラス +/
+class BitmapLight : ColorDrawBuf
+{
+    public:
+        /+ 通常のBMPから生成 +/
+        this ( BMP src )
+        {
+            super( src.width, src.height );
+            foreach ( y; 0 .. src.height )
+                foreach ( x; 0 .. src.width )
+                    _buf[y*_dx+x] = src[x,y].toHex;
+        }
+}
