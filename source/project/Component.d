80a7b076bc8f08964adee1a2910e45e6e390eb54
aoitofu <aoitofu@dr.com>
2017-06-13 19:41:03 +0900

    オブジェクトのレンダリング情報をまとめるメソッドを追加

--- source/project/Component.d
+++ source/project/Component.d
@@ -5,14 +5,49 @@
  + Please see /LICENSE.                                         +
  + ------------------------------------------------------------ +/
 module cafe.project.Component;
-import cafe.project.timeline.Timeline;
+import cafe.project.timeline.Timeline,
+       cafe.project.ObjectPlacingInfo,
+       cafe.project.RenderingInfo,
+       cafe.renderer.graphics.Bitmap,
+       cafe.renderer.World;
+import std.algorithm;
 
-debug = 0;
+debug = 1;
 
 /+ プロジェクト内のコンポーネントデータ +
  + AULでいうシーン                      +/
 class Component
 {
     private:
-        Timeline timeline;
+        Timeline tl;
+
+    public:
+        @property timeline () { return tl; }
+
+        this ()
+        {
+            tl = new Timeline;
+        }
+
+        /+ Worldを生成 +/
+        World generate ( FrameAt f )
+        {
+            auto rinfo   = new RenderingInfo;
+            auto objects = timeline.objectsAtFrame(f).sort!
+                ( (a, b) => a.place.layer.value < b.place.layer.value );
+            objects.each!( x => x.generate( rinfo ) );
+            return rinfo.renderingStage;
+        }
+
+        /+ 画像を生成 +/
+        BMP render ( FrameAt f )
+        {
+            throw new Exception( "Not Implemented" );
+        }
+
+        debug (1) unittest {
+            auto hoge = new Component;
+            assert( hoge.generate( new FrameAt(0) ).polygons.length == 0 );
+            // hoge.render( new FrameAt(0) );
+        }
 }
