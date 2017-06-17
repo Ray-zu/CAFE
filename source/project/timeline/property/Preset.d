5157257b4abcac85eefac5f7a567623acfdfcde9
aoitofu <aoitofu@dr.com>
2017-06-17 17:19:22 +0900

    従来のPropertyBaseからLimitedPropertyに修正

--- source/project/timeline/property/Preset.d
+++ source/project/timeline/property/Preset.d
@@ -6,6 +6,7 @@
  + ------------------------------------------------------------ +/
 module cafe.project.timeline.property.Preset;
 import cafe.project.timeline.property.PropertyList,
+       cafe.project.timeline.property.LimitedProperty,
        cafe.project.timeline.property.Property,
        cafe.project.ObjectPlacingInfo;
 
@@ -13,23 +14,24 @@ import cafe.project.timeline.property.PropertyList,
 abstract class PropertyPresets
 {
     public:
+        /+ 数値プロパティに関しては全てLimitedPropertyを使います +/
 
         /+ 位置指定プロパティ +/
         static void position ( PropertyList a, FrameLength f )
         {
-            a["X"] = new PropertyBase!float( f, 0 );
-            a["Y"] = new PropertyBase!float( f, 0 );
-            a["Z"] = new PropertyBase!float( f, 0 );
+            a["X"] = new LimitedProperty!float( f, 0 );
+            a["Y"] = new LimitedProperty!float( f, 0 );
+            a["Z"] = new LimitedProperty!float( f, 0 );
         }
 
         /+ 回転指定プロパティ +/
         static void rotation ( PropertyList a, FrameLength f )
         {
-            a["RotationX"] = new PropertyBase!float( f, 0 );
-            a["RotationY"] = new PropertyBase!float( f, 0 );
-            a["RotationZ"] = new PropertyBase!float( f, 0 );
-            a["CenterX"]   = new PropertyBase!float( f, 0 );
-            a["CenterY"]   = new PropertyBase!float( f, 0 );
-            a["CenterZ"]   = new PropertyBase!float( f, 0 );
+            a["RotationX"] = new LimitedProperty!float( f, 0 );
+            a["RotationY"] = new LimitedProperty!float( f, 0 );
+            a["RotationZ"] = new LimitedProperty!float( f, 0 );
+            a["CenterX"]   = new LimitedProperty!float( f, 0 );
+            a["CenterY"]   = new LimitedProperty!float( f, 0 );
+            a["CenterZ"]   = new LimitedProperty!float( f, 0 );
         }
 }
