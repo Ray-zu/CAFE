dd1184120582472092ac8041e7f85f444319b3c4
aoitofu <aoitofu@dr.com>
2017-06-23 19:00:11 +0900

    Timelineクラスに指定した範囲のオブジェクト配列を返す演算子を追加

--- source/project/timeline/Timeline.d
+++ source/project/timeline/Timeline.d
@@ -10,7 +10,7 @@ import cafe.project.ObjectPlacingInfo,
 import std.algorithm,
        std.array;
 
-debug = 0;
+debug = 1;
 
 /+ タイムラインデータ +/
 class Timeline
@@ -48,6 +48,25 @@ class Timeline
             return objects.filter!( x => x.place.frame.isInRange(f) ).array;
         }
 
+        /+ this[f] : フレーム期間fのオブジェクト配列を返す +/
+        auto opIndex ( FramePeriod f )
+        {
+            return objects.filter!( x => x.place.frame.isWhileRange(f) ).array;
+        }
+
+        /+ this[f1,f2] : f1~f2期間のオブジェクト配列を返す +/
+        auto opIndex ( FrameAt f1, FrameAt f2 )
+        {
+            return this[ new FramePeriod( length, f1, f2 ) ];
+        }
+
+        /+ this[f1,f2,l] : f1~f2期間中からレイヤlに配置されている +
+           オブジェクトの配列を返す                               +/
+        auto opIndex ( FrameAt f1, FrameAt f2, LayerId l )
+        {
+            return this[f1,f2].filter!( x => x.place.layer.value == l.value ).array;
+        }
+
         debug (1) unittest {
             import cafe.project.timeline.custom.NullObject;
             auto hoge = new Timeline( new FrameLength( 10 ) );
@@ -58,5 +77,7 @@ class Timeline
             hoge += obj1;
 
             assert( hoge[new FrameAt(0)].length == 1 );
+            assert( hoge[new FrameAt(0),new FrameAt(1),new LayerId(0)].length == 1 );
+            assert( hoge[new FrameAt(0),new FrameAt(1),new LayerId(1)].length == 0 );
         }
 }
