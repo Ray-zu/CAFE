e7cf12d08efb3d36f2bba3a8508196d8150061e4
aoitofu <aoitofu@dr.com>
2017-06-24 21:40:12 +0900

    mediaモジュールを追加

---
+++ source/media/decoder/Bitmap.d
@@ -0,0 +1,7 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.media.decoder.Decoder;
