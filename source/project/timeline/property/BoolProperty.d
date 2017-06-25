aed1679a039b7e1cbbd20b35998c48b8841d9f77
aoitofu <aoitofu@dr.com>
2017-06-25 09:47:59 +0900

    Boolプロパティを追加

---
+++ source/project/timeline/property/BoolProperty.d
@@ -0,0 +1,59 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.project.timeline.property.BoolProperty;
+import cafe.project.timeline.property.Property,
+       cafe.project.ObjectPlacingInfo;
+import std.algorithm,
+       std.conv,
+       std.exception,
+       std.json,
+       std.string;
+
+debug = 1;
+
+/+ Bool型のプロパティ +/
+class BoolProperty : PropertyBase!bool
+{
+    protected:
+        override @property string typeToString ()
+        {
+            return "bool";
+        }
+
+    public:
+        override @property Property copy ()
+        {
+            return new BoolProperty( this );
+        }
+
+        this ( BoolProperty src )
+        {
+            super( src );
+        }
+
+        this ( FrameLength f, bool v )
+        {
+            super( f, v );
+        }
+
+        this ( JSONValue[] j, FrameLength f, bool v )
+        {
+            super( j, f, v );
+        }
+
+        override void setString ( FrameAt f, string v )
+        {
+            if ( v.toLower != "true" ) v = "false";
+            super.setString( f, v );
+        }
+
+        debug (1) unittest {
+            auto hoge = new BoolProperty( new FrameLength(5), true );
+            auto hoge2 = Property.create( hoge.json, hoge.frame );
+            assert( hoge.json.to!string == hoge2.json.to!string );
+        }
+}
