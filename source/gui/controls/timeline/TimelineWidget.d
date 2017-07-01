169dbee25dbd5a004e5f2dba921321b176a7d95b
aoitofu <aoitofu@dr.com>
2017-07-01 13:20:30 +0900

    タイムラインウィジェットを追加

---
+++ source/gui/controls/timeline/TimelineWidget.d
@@ -0,0 +1,25 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.gui.controls.timeline.TimelineWidget;
+import cafe.gui.controls.timeline.TimelineEditor;
+import dlangui;
+
+/+ タイムラインウィジェット +/
+class TimelineWidget : VerticalLayout
+{
+    private:
+        TimelineEditor tl_editor;
+
+    public:
+        this ( string id = "" )
+        {
+            super( id );
+            addChild( parseML( q{
+                ScrollBar { id:"hscroll"; Orientation:Horizontal }
+            } ) );
+        }
+}
