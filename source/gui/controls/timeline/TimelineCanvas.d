10abbfd2dc8746f4f0961fc517c288f147771419
aoitofu <aoitofu@dr.com>
2017-07-01 13:39:16 +0900

    TimelineCanvasクラスを追加

---
+++ source/gui/controls/timeline/TimelineCanvas.d
@@ -0,0 +1,21 @@
+/+ ------------------------------------------------------------ +
+ + Author : aoitofu <aoitofu@dr.com>                            +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.gui.controls.timeline.TimelineCanvas;
+import dlangui,
+       dlangui.widgets.metadata;
+
+mixin( registerWidgets!TimelineCanvas );
+
+class TimelineCanvas : Widget
+{
+    private:
+    public:
+        this ( string id = "" )
+        {
+            super( id );
+        }
+}
