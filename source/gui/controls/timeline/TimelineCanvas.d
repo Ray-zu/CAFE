4e5b71d702ec1babe95bdc5239354a7d074ba5b4
aoitofu <aoitofu@dr.com>
2017-07-02 17:04:18 +0900

    表示中のライン情報の配列を返すメソッドを追加

--- source/gui/controls/timeline/TimelineCanvas.d
+++ source/gui/controls/timeline/TimelineCanvas.d
@@ -5,9 +5,12 @@
  + Please see /LICENSE.                                         +
  + ------------------------------------------------------------ +/
 module cafe.gui.controls.timeline.TimelineCanvas;
-import cafe.gui.controls.timeline.TimelineEditor;
+import cafe.gui.controls.timeline.TimelineEditor,
+       cafe.gui.controls.timeline.Line;
 import dlangui,
        dlangui.widgets.metadata;
+import std.conv,
+       std.math;
 
 mixin( registerWidgets!TimelineCanvas );
 
@@ -16,10 +19,25 @@ class TimelineCanvas : Widget
     private:
         TimelineEditor tl_editor;
 
+        /+ 表示中のライン情報の配列を返す +/
+        auto showingLineInfo ()
+        {
+            Line[] result = [];
+            uint i = topLineIndex.trunc.to!uint;
+            auto h = 0;
+            do {
+                result ~= tl_editor.lineInfo( i++ );
+                h += (result[$-1].height * lineHeight).to!int;
+            } while ( h < height );
+            return result;
+        }
+
     public:
         uint  startFrame;
         uint  pageWidth;
+
         float topLineIndex;
+        uint  lineHeight;
 
         @property timeline ( TimelineEditor tl )
         {
@@ -39,5 +57,10 @@ class TimelineCanvas : Widget
         override void measure ( int w, int h )
         {
             measuredContent( w, h, w, h );
+        }
+
+        override void onDraw ( DrawBuf b )
+        {
+            super.onDraw( b );
         }
 }
