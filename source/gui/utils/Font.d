47c919ddba9ed65602abad279c0d9f6393090647
aoitofu <aoitofu@dr.com>
2017-07-05 17:38:02 +0900

    新しい文字列病が関数を追加

--- source/gui/utils/Font.d
+++ source/gui/utils/Font.d
@@ -15,3 +15,11 @@ void drawCenteredText ( Font f, DrawBuf b, int x, int y, string text, uint col )
     auto sz = f.textSize( dtext );
     f.drawText( b, x-sz.x/2,y-sz.y/2, dtext, col );
 }
+
+/+ 横方向に左揃えで縦方向に中央揃えで描画 +/
+void drawLeftCenteredText ( Font f, DrawBuf b, int x, int y, string text, uint col )
+{
+    auto dtext = text.to!dstring;
+    auto sz = f.textSize( dtext );
+    f.drawText( b, x,y-sz.y/2, dtext, col );
+}
