d7f03319b4483b823a8d853675200807a9ef68fb
aoitofu <aoitofu@dr.com>
2017-07-11 22:32:02 +0900

    オブジェクトを伸ばした時の処理を追加

--- source/gui/controls/timeline/ObjectEditor.d
+++ source/gui/controls/timeline/ObjectEditor.d
@@ -11,6 +11,7 @@ import cafe.project.ObjectPlacingInfo,
        cafe.project.timeline.property.Property,
        cafe.project.timeline.property.MiddlePoint;
 import std.algorithm,
+       std.conv,
        std.exception;
 
 debug = 1;
@@ -30,16 +31,25 @@ class ObjectEditor
 
         /+ 中間点を破壊しながらリサイズ                +
          + 縮めた場合は後ろの中間点を消し、            +
-         + 伸ばした場合は後ろの中間点のみを伸ばします。+/
+         + 伸ばした場合は後ろの中間点からのみを伸ばします。+/
         void resizeDestroy ( int len )
         {
             enforce( len > 0, "Length must be 1 or more." );
+            obj.place.frame.length.value = len.to!uint;
 
             void proc ( Property prop )
             {
                 auto cut_mp = prop.middlePoints.countUntil
                     !( x => x.frame.start.value >= len );
-                // TODO
+
+                if ( cut_mp == -1 ) {
+                    // 伸ばされたときは最後の中間点からを伸ばす
+                    auto mp = prop.middlePoints[$-1];
+                    mp.frame.length.value =
+                        (len - mp.frame.start.value.to!int).to!uint;
+                } else {
+                    //TODO 
+                }
             }
 
             obj.propertyList.properties.values.each!proc;
