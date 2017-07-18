5035b50fd8d11bc87a1eb20d628d7fbf47ac1ccd
aoitofu <aoitofu@dr.com>
2017-07-18 22:30:12 +0900

    メインフレームの初期化処理を追加

--- source/gui/controls/MainFrame.d
+++ source/gui/controls/MainFrame.d
@@ -5,10 +5,37 @@
  + Please see /LICENSE.                                         +
  + ------------------------------------------------------------ +/
 module cafe.gui.controls.MainFrame;
+import std.conv,
+       std.format;
 import dlangui;
 
 class MainFrame : AppFrame
 {
+    enum AppName = "CAFEditor";
+    enum AppVer  = "0.99 beta";
+    enum AppText = "%s %s".format( AppName, AppVer );
+
+    private:
+        MenuItem top_menu;
+
+    protected:
+        override void initialize ()
+        {
+            _appName = AppText;
+            super.initialize();
+        }
+
+        override MainMenu createMainMenu ()
+        {
+            top_menu = new MenuItem;
+            auto file = new MenuItem( new Action( 1, "MENU_FILE" ) );
+            with ( file ) {
+                add( new Action( 0 ) );
+            }
+            top_menu.add( file );
+            return new MainMenu( top_menu );
+        }
+
     public:
         this ()
         {
