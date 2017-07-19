c516468ab362abf6eeae0980acd81833776b73fb
aoitofu <aoitofu@dr.com>
2017-07-19 16:49:27 +0900

    再生制御アクションを追加

--- source/gui/Actions.d
+++ source/gui/Actions.d
@@ -14,7 +14,15 @@ enum EditorActions : int
     ProjectSave,
     ProjectSaveAs,
     ProjectClose,
-    ProjectExit
+    ProjectExit,
+
+    Play,
+    Pause,
+    Stop,
+    MoveBehind,
+    MoveAHead,
+    ShiftBehind,
+    ShiftAHead
 }
 
 const Action_ProjectNew    = new Action( cast(int)EditorActions.ProjectNew   , "ProjectNew"    );
@@ -22,3 +30,11 @@ const Action_ProjectOpen   = new Action( cast(int)EditorActions.ProjectOpen  , "
 const Action_ProjectSave   = new Action( cast(int)EditorActions.ProjectSave  , "ProjectSave"  , "save" );
 const Action_ProjectSaveAs = new Action( cast(int)EditorActions.ProjectSaveAs, "ProjectSaveAs", "save_again" );
 const Action_ProjectClose  = new Action( cast(int)EditorActions.ProjectClose , "ProjectClose"  );
+
+const Action_Play        = new Action( cast(int)EditorActions.Play       , "Play"       , "play"         );
+const Action_Pause       = new Action( cast(int)EditorActions.Pause      , "Pause"      , "pause"        );
+const Action_Stop        = new Action( cast(int)EditorActions.Stop       , "Stop"       , "stop"         );
+const Action_MoveBehind  = new Action( cast(int)EditorActions.MoveBehind , "MoveBehind" , "move_behind"  );
+const Action_MoveAHead   = new Action( cast(int)EditorActions.MoveAHead  , "MoveAHead"  , "move_ahead"   );
+const Action_ShiftBehind = new Action( cast(int)EditorActions.ShiftBehind, "ShiftBehind", "shift_behind" );
+const Action_ShiftAHead  = new Action( cast(int)EditorActions.ShiftAHead , "ShiftAHead" , "shift_ahead"  );
