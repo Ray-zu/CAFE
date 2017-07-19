1a7ae92fb85f583e1b3a453c254d9be286765476
aoitofu <aoitofu@dr.com>
2017-07-19 16:49:37 +0900

    ツールバーに再生制御ボタンを追加

--- source/gui/controls/MainFrame.d
+++ source/gui/controls/MainFrame.d
@@ -44,10 +44,21 @@ class MainFrame : AppFrame
         override ToolBarHost createToolbars ()
         {
             auto host = new ToolBarHost;
-            auto bar  = host.getOrAddToolbar( "Standard" );
+            auto bar  = host.getOrAddToolbar( "File" );
             with ( bar ) {
                 addButtons( Action_ProjectSave   );
                 addButtons( Action_ProjectSaveAs );
+            }
+            with ( bar = host.getOrAddToolbar( "Control" ) ) {
+                addButtons( Action_Play        );
+                addButtons( Action_Pause       );
+                addButtons( Action_Stop        );
+
+                addButtons( Action_MoveBehind );
+                addButtons( Action_MoveAHead  );
+
+                addButtons( Action_ShiftBehind );
+                addButtons( Action_ShiftAHead  );
             }
             return host;
         }
