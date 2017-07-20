1eec3013e687e190611f649da210073a04566744
aoitofu <aoitofu@dr.com>
2017-07-20 16:40:39 +0900

    BMPViewerをdlanguiのウィジェットに登録

--- source/gui/controls/BMPViewer.d
+++ source/gui/controls/BMPViewer.d
@@ -7,7 +7,10 @@
 module cafe.gui.controls.BMPViewer;
 import cafe.gui.BitmapLight,
        cafe.renderer.graphics.Bitmap;
-import dlangui;
+import dlangui,
+       dlangui.widgets.metadata;
+
+mixin( registerWidgets!BMPViewer );
 
 /+ BMPを表示するウィジェット +/
 class BMPViewer : Widget
@@ -23,15 +26,16 @@ class BMPViewer : Widget
             invalidate;
         }
 
-        this ( string id, DrawBuf bmp = null )
+        this ( string id = "" )
         {
-            super( id );
-            bitmap = bmp;
+            this( id, null );
         }
 
         this ( string id, BMP bmp )
         {
-            this( id, new BitmapLight( bmp ) );
+            super( id );
+            bitmap = bmp ?
+                new BitmapLight( bmp ) : null;
         }
 
         override void onDraw ( DrawBuf b )
