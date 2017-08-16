39b625cec0ab9bb102de2450e5e3c8ee93ecc8c0
SEED264 <seedx264@gmail.com>
2017-08-16 16:00:37 +0900

    Polygonクラスのvertexを分解、indexと描画モードの追加

--- source/renderer/polygon/Polygon.d
+++ source/renderer/polygon/Polygon.d
@@ -1,5 +1,6 @@
 /+ ------------------------------------------------------------ +
  + Author : aoitofu <aoitofu@dr.com>                            +
+ +          SEED264                                             +
  + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
  + ------------------------------------------------------------ +
  + Please see /LICENSE.                                         +
@@ -7,8 +8,9 @@
 module cafe.renderer.polygon.Polygon;
 import cafe.renderer.graphics.Bitmap,
        cafe.renderer.polygon.Material,
-       cafe.renderer.polygon.Vector,
        cafe.renderer.polygon.Vertex;
+import derelict.opengl3.gl3;
+import gl3n.linalg;
 
 /+ ポリゴンのデータ +/
 class Polygon
@@ -16,17 +18,27 @@ class Polygon
     private:
         BMP       tex;
         Material  mat;
-        Vertex[4] verts;    // 左上から時計回りに指定
+        vec3[]    pos;    // 三次元座標
+        uint[]    vidx;   // 頂点インデックス
+        vec2[]    uvpos;  // UV座標
+        GLenum    dmode;  // ポリゴンの描画モード
         /+ TODO エフェクト情報 +/
 
     public:
         @property texture  () { return tex; }
-        @property vertexes () { return verts; }
+        @property position () { return pos; }
+        @property vindex () { return vidx; }
+        @property uv () { return uvpos; }
+        @property drawmode () { return dmode; }
+        @property drawmode (GLenum a) { dmode = a; }
         @property material () { return mat; }
 
-        this ( BMP t, Vertex[4] v )
+        this ( BMP t, vec3[] p, uint[] vi, vec2[] uv, GLenum m = GL_TRIANGLES )
         {
             tex = t;
-            verts = v;
+            pos = p;
+            vidx = vi;
+            uvpos = uv;
+            dmode = m;
         }
 }
