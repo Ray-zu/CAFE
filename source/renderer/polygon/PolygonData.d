7fbed4ef82b7dbb7c622995c143e9441566ac90b
SEED264 <seedx264@gmail.com>
2017-08-19 00:50:02 +0900

    Transform構造体を追加

---
+++ source/renderer/polygon/PolygonData.d
@@ -0,0 +1,28 @@
+/+ ------------------------------------------------------------ +
+ + Author : SEED264                                             +
+ + This is part of CAFE ( https://github.com/aoitofu/CAFE ).    +
+ + ------------------------------------------------------------ +
+ + Please see /LICENSE.                                         +
+ + ------------------------------------------------------------ +/
+module cafe.renderer.polygon.PolygonData;
+import gl3n.linalg,
+       gl3n.math;
+
+struct Transform
+{
+    private:
+        vec3 Translate;
+        vec3 Rotate;
+        vec3 Scale;
+    public:
+        @property mat()
+        {
+            mat4 tm = mat4.identity;
+            tm.scale(Scale.x, Scale.y, Scale.z);
+            tm.rotate(radians(Rotate.z), vec3( 0.0, 0.0, 1.0));
+            tm.rotate(radians(Rotate.y), vec3( 0.0, 1.0, 0.0));
+            tm.rotate(radians(Rotate.x), vec3(-1.0, 0.0, 0.0));
+            tm.translate(Translate);
+            return tm;
+        }
+}
