bb688b0ed8beb60ea00ed9d950a6a835ebdaaa49
SEED264 <seedx264@gmail.com>
2017-08-19 01:06:30 +0900

    PolygonにTransformを追加

--- source/renderer/polygon/Polygon.d
+++ source/renderer/polygon/Polygon.d
@@ -8,7 +8,8 @@
 module cafe.renderer.polygon.Polygon;
 import cafe.renderer.graphics.Bitmap,
        cafe.renderer.polygon.Material,
-       cafe.renderer.polygon.Vertex;
+       cafe.renderer.polygon.Vertex,
+       cafe.renderer.polygon.PolygonData;
 import derelict.opengl3.gl3;
 import gl3n.linalg;
 
@@ -22,6 +23,7 @@ class Polygon
         uint[]    vidx;   // 頂点インデックス
         vec2[]    uvpos;  // UV座標
         GLenum    dmode;  // ポリゴンの描画モード
+        Transform trans;  // トランスフォームデータ
         /+ TODO エフェクト情報 +/
 
     public:
@@ -32,13 +34,16 @@ class Polygon
         @property drawmode () { return dmode; }
         @property drawmode (GLenum a) { dmode = a; }
         @property material () { return mat; }
+        @property transform () { return trans; }
+        @property transform (Transform t) { trans = t; }
 
-        this ( BMP t, vec3[] p, uint[] vi, vec2[] uv, GLenum m = GL_TRIANGLES )
+        this ( BMP t, vec3[] p, uint[] vi, vec2[] uv, Transform t, GLenum m = GL_TRIANGLES )
         {
             tex = t;
             pos = p;
             vidx = vi;
             uvpos = uv;
+            trans = t;
             dmode = m;
         }
 }
